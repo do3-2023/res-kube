60e79e65749d00cd7ef35bf6ab6b6c18fa555b6f
ESPIE <remi.espie@etu.umontpellier.fr>
2023-06-28 17:52:44 +0200

    feat: add api

---
+++ res-api/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: 'db',
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/healtz', (req, res) => {
+    pool.query('SELECT NOW()', (err, result) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        res.status(200).send(result.rows)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
