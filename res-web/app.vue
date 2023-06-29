518672c929b5e9e58038ec3f9dcf2e69e4b5f7b4
ESPIE <remi.espie@etu.umontpellier.fr>
2023-06-29 14:31:28 +0200

    feat: display data from DB in front

--- res-web/app.vue
+++ res-web/app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>Hello, you !</p>
+    <p>Our backend tells us it is {{ data[0].now }} </p>
   </div>
 </template>
+
+<script setup>
+
+const {data, error} = await useFetch('http://api.res-back.svc.cluster.local:8080/healtz')
+
+</script>
